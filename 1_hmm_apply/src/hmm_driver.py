069cb42149117cdbce1546f4472c9cf80149b425
wangxin <wangxin4@iie.ac.cn>
2017-10-29 21:52:57 +0800

    init repository

--- 1_hmm_apply/src/hmm_driver.py
+++ 1_hmm_apply/src/hmm_driver.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     option = 'training'
     if option == 'training':
         io_obj = DataIO()
-        exp_data = io_obj.load_raw_data('C:\\Users\\Administrator\\Desktop\\raw_data.txt')
+        exp_data = io_obj.load_raw_data('../data/raw_data.txt')
         exp_data.data_cutting(8,2)
         io_obj.persit_data(exp_data.get_train_data(),'../data/train_data.txt')
         io_obj.persit_data(exp_data.get_test_data(), '../data/test_data.txt')
