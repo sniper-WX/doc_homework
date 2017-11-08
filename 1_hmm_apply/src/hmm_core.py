c70219ac66a1b5f04df38e6afdef039f91d0ded9
wangxin <wangxin4@iie.ac.cn>
2017-11-08 22:58:05 +0800

    init repository

--- 1_hmm_apply/src/hmm_core.py
+++ 1_hmm_apply/src/hmm_core.py
@@ -1,2 +1,106 @@
-import numpy
+import numpy as  np
+
+
+class HMMModel(object):
+    def __init__(self):
+        self.__hiden_state_transfer= {}
+        self.__hiden_visible_transfer = {}
+        self.__init_hiden = {}
+
+
+    def train(self, train_data, pos_tags):
+        init_state_cnt = {}#π
+        hiden_visible_cnt = {}#B
+        hiden_transfer_cnt = {}#A
+        #初始化初始状态概率
+        for pos_tag in pos_tags:
+            self.__init_hiden[pos_tag] = float(0)
+            self.__hiden_state_transfer[pos_tag] = {}
+            self.__hiden_visible_transfer[pos_tag] = {}
+
+            hiden_transfer_cnt[pos_tag] = {}
+            hiden_visible_cnt[pos_tag] = {}
+            for pos_tag_1 in pos_tags:
+                hiden_transfer_cnt[pos_tag][pos_tag_1] = 0
+                self.__hiden_state_transfer[pos_tag][pos_tag_1] = float(0)
+
+        record_cnt = 0
+        for train_record in train_data:
+            record_cnt += 1
+            is_start = True
+            last_pos_tag = ''
+            for word in train_record:
+                word_str = word['word']
+                pos_tag = word['pos_tag']
+                if is_start == True:
+                    is_start = False
+                    # 统计初始状态
+                    if pos_tag not in init_state_cnt:
+                        init_state_cnt[pos_tag] = 1
+                    else:
+                        init_state_cnt[pos_tag] += 1
+                    last_pos_tag = pos_tag
+                    continue
+
+                #统计隐藏状态到观测状态的转移概率
+                if word_str not in hiden_visible_cnt[pos_tag]:
+                    hiden_visible_cnt[pos_tag][word_str] = 1
+                    self.__hiden_visible_transfer[pos_tag][word_str] = float(0)
+                else:
+                    hiden_visible_cnt[pos_tag][word_str] += 1
+
+                # 统计隐藏状态之间的转换
+                hiden_transfer_cnt[last_pos_tag][pos_tag] += 1
+
+            if record_cnt % 10000 == 0:
+                print('Trained %s records'% record_cnt)
+        print("--------------------finished counting----------------------")
+        #计算初始概率
+        print("--------------------compute init state_cnt ----------------------")
+        total_count = float(0)
+        for state in init_state_cnt:
+            total_count += init_state_cnt[state]
+        for state in init_state_cnt:
+            self.__init_hiden[state] = init_state_cnt[state]/total_count
+            print(' %s::%s' % (state, self.__init_hiden[state]))
+
+        #计算隐藏到观测状态概率
+        print("--------------------compute hiden_visible_cnt ----------------------")
+        for pos_tag in hiden_visible_cnt:
+            pos_total_count = 0
+            print('pos_tag:%s ===>'%pos_tag)
+            for word_str in hiden_visible_cnt[pos_tag]:
+                pos_total_count += hiden_visible_cnt[pos_tag][word_str]
+            for word_str in hiden_visible_cnt[pos_tag]:
+                current_word_cnt = hiden_visible_cnt[pos_tag][word_str]
+                self.__hiden_visible_transfer[pos_tag][word_str] = float(current_word_cnt)/pos_total_count
+                print(' %s::%s' % (word_str, self.__hiden_visible_transfer[pos_tag][word_str]),end='')
+            print("")
+
+        # 计算隐藏状态转移概率
+        print("--------------------compute hiden_transfer_cnt ----------------------")
+        for pos_tag_from in hiden_transfer_cnt:
+            cur_pos_total_cnt = 0
+            print('pos_tag_from:%s ===>'%pos_tag_from)
+            for pos_tag_to in hiden_transfer_cnt[pos_tag_from]:
+                cur_pos_total_cnt += hiden_transfer_cnt[pos_tag_from][pos_tag_to]
+            if cur_pos_total_cnt == 0:
+                continue
+            for pos_tag_to in hiden_transfer_cnt[pos_tag_from]:
+                current_pos_cnt = hiden_transfer_cnt[pos_tag_from][pos_tag_to]
+                self.__hiden_state_transfer[pos_tag_from][pos_tag_to] = float(current_pos_cnt)/cur_pos_total_cnt
+                print(' %s::%s'%(pos_tag_to, self.__hiden_state_transfer[pos_tag_from][pos_tag_to]),end='')
+            print("")
+
+
+
+
+
+
+    def test(self,test_data):
+
+        pass
+
+    def __viterbi_search(self):
+        pass
 
--- 1_hmm_apply/src/hmm_driver.py
+++ 1_hmm_apply/src/hmm_driver.py
@@ -1,14 +1,19 @@
 from dataIO import DataIO
+from hmm_core import HMMModel
 
 if __name__ == '__main__':
     option = 'training'
+    model = HMMModel()
     if option == 'training':
         io_obj = DataIO()
-        exp_data = io_obj.load_raw_data('../data/raw_data.txt')
+        #exp_data = io_obj.load_raw_data('../data/raw_data.txt')
+        exp_data = io_obj.load_raw_data('../data/raw_test_data.txt')
+
         exp_data.data_cutting(8,2)
         io_obj.persit_data(exp_data.get_train_data(),'../data/train_data.txt')
         io_obj.persit_data(exp_data.get_test_data(), '../data/test_data.txt')
-        io_obj.persit_data(exp_data.get_all_tags, '../data/tags_in_use.txt')
+        io_obj.persit_data(exp_data.get_all_tags(), '../data/tags_in_use.txt')
+        model.train(exp_data.get_train_data(), exp_data.get_all_tags())
 
     elif option == 'testing':
         pass
